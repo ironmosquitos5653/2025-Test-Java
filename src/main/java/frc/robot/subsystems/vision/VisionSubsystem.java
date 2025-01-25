d2395901e5a00c1812ffd31bdba615cff5a6116f
Iron Mosquitos <119903571+ironmosquitos5653@users.noreply.github.com>
2025-01-25 12:11:22 -0600

    Initial check-in
    Initial Check-in

---
+++ src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
@@ -0,0 +1,94 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.vision;
+
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.drive.Drive;
+
+public class VisionSubsystem extends SubsystemBase {
+  private Drive m_driveSubsystem;
+  /** Creates a new VisionSubsystem. */
+  private final Field2d field2d = new Field2d();
+
+  public VisionSubsystem(Drive driveSubsystem) {
+    m_driveSubsystem = driveSubsystem;
+    ShuffleboardTab tab = Shuffleboard.getTab("Vision");
+    tab.addString("Pose", this::getFomattedPose).withPosition(0, 0).withSize(2, 0);
+    tab.add("Field", field2d).withPosition(2, 0).withSize(6, 4);
+  }
+
+  @Override
+  public void periodic() {
+
+    boolean useMegaTag2 = !true; // set to false to use MegaTag1
+    boolean doRejectUpdate = false;
+    if (useMegaTag2 == false) {
+      LimelightHelpers.PoseEstimate mt1 =
+          LimelightHelpers.getBotPoseEstimate_wpiBlue("limelight-one");
+
+      if (mt1.tagCount == 1 && mt1.rawFiducials.length == 1) {
+        if (mt1.rawFiducials[0].ambiguity > .7) {
+          doRejectUpdate = true;
+        }
+        if (mt1.rawFiducials[0].distToCamera > 3) {
+          doRejectUpdate = true;
+        }
+      }
+      if (mt1.tagCount == 0) {
+        doRejectUpdate = true;
+      }
+
+      if (!doRejectUpdate) {
+        SmartDashboard.putString("mt1", getFomattedPose(mt1.pose));
+        m_driveSubsystem.addVisionMeasurement(
+            mt1.pose, mt1.timestampSeconds, VecBuilder.fill(.5, .5, .5));//9999999));
+        updateField();
+      }
+    } else if (useMegaTag2 == true) {
+      LimelightHelpers.SetRobotOrientation(
+          "limelight-one", m_driveSubsystem.getPose().getRotation().getDegrees(), 0, 0, 0, 0, 0);
+      LimelightHelpers.PoseEstimate mt2 =
+          LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight-one");
+
+      if (mt2 != null) {
+
+        if (Math.abs(m_driveSubsystem.getTurnRate())
+            > 720) // if our angular velocity is greater than 720 degrees per second, ignore vision
+        // updates
+        {
+          doRejectUpdate = true;
+        }
+        if (mt2.tagCount == 0) {
+          doRejectUpdate = true;
+        }
+        if (!doRejectUpdate) {
+          m_driveSubsystem.addVisionMeasurement(
+              mt2.pose, mt2.timestampSeconds, VecBuilder.fill(.5, .5, 9999999));
+          updateField();
+        }
+      }
+    }
+  }
+
+  public void updateField() {
+    field2d.setRobotPose(m_driveSubsystem.getPose());
+  }
+
+  private String getFomattedPose() {
+    return getFomattedPose(m_driveSubsystem.getPose());
+  }
+
+  private String getFomattedPose(Pose2d pose) {
+
+    return String.format(
+        "(%.2f, %.2f) %.2f degrees", pose.getX(), pose.getY(), pose.getRotation().getDegrees());
+  }
+}
